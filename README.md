9042bf0424a18b222450dd9810e85b678387c94a
Iulia Ilies <iulia.ilies01@gmail.com>
2024-11-23 18:26:11 +0200

    Add auth microfrontend and protected routing

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 This is a POC of a microfrontend architecture, consisting of:
 - a container
-- microfrontend #1, which represents a list of expenses on which the user can perform CRUD operations
-- microfrontend #2, which represents a settings&notifications area, where users can set a weekly expenses limit and check their spending summaries.
+- microfrontend #1, handling authentication
+- microfrontend #3, which contains a list of expenses on which the user can perform CRUD operations; it also allows the user to change their weekly spending limit
+- microfrontend #3, which represents a notification area, where users can check their spending summaries.
--- src/iulia-soa-root-config.ts
+++ src/iulia-soa-root-config.ts
@@ -6,13 +6,29 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
+const isAuthenticated = () => !!localStorage.getItem("jwtToken");
+
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
+    const currentPath = window.location.pathname;
+
+    if (isAuthenticated() && currentPath !== "/expenses") {
+      window.history.pushState(null, "", "/expenses");
+      return System.import("@iulia-soa/mfe-expenses");
+    }
+
+    if (!isAuthenticated() && currentPath !== "/login") {
+      window.history.pushState(null, "", "/login");
+      return System.import("@iulia-soa/mfe-auth");
+    }
+
     return System.import(name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
